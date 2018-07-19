4fdad8c0eba199988a0e0607eee70c515e176fd1
ignas26 <ignas.zlatkus@gmail.com>
2018-07-19 18:06:39 +0300

    added ClipsRestOf component w/css, for related clips list inside of each clip posts

--- src/components/ClipsPosts.js
+++ src/components/ClipsPosts.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import {URL} from '../types/allTypes'
 import Header from './ClipsHeader'
+import ClipsRestOf from './reusables/ClipsRestOf'
 
 class ClipsPosts extends Component{
 state={
   clips:[],
-  team:[]
+  team:[],
+  teams:[],
+  restOf:[]
 };
 
   componentWillMount(){
@@ -16,10 +19,27 @@ state={
         this.setState({
           clips:post,
           team:response.data
-        })
+        });
+        this.restOfClips();
       })
     })
   }
+
+  restOfClips=()=>{
+    axios.get(`${URL}/teams`)
+        .then(response=>{
+
+let teams = response.data;
+
+          axios.get(`${URL}/clips?q=${this.state.team[0].city}`)
+              .then(response=>{
+this.setState({
+  teams,
+  restOf:response.data
+})
+              })
+    });
+  };
 
   render(){
 const clips = this.state.clips;
@@ -28,7 +48,20 @@ const team = this.state.team;
     return(
         <div>
           <Header teamdata={team[0]}/>
-          clips
+          <div className='wrapper'>
+<h1>{clips.title}</h1>
+            <iframe
+              title='player'
+              width='100%'
+              height='300px'
+              src={`https://www.youtube.com/embed/${clips.url}`}
+            >
+            </iframe>
+          </div>
+          <ClipsRestOf
+          data={this.state.restOf}
+          teams={this.state.teams}
+          />
         </div>
     )
   }
---
+++ src/components/reusables/ClipsRestOf.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ClipsTemplate from './ClipsTemplate';
+
+
+const ClipsRestOf =(props)=>(
+
+<div className='wrapper'>
+  <ClipsTemplate
+  data={props.data}
+  teams={props.teams}
+  />
+<h1>aaa</h1>
+</div>
+
+);
+
+export default ClipsRestOf
